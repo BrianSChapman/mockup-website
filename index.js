1b5243f4a6cc3d767fd282517f57f8ed2e88732a
Brian Chapman <chapman.brian86@gmail.com>
2023-02-16 19:35:17 -0500

    Added more images and beginning to flesh out main layout of page before adding functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+
+// jQuery initialization for the hamburger mobile menu and the hero-image slider
+
+//   $(document).ready(function() {
+//     $('.sidenav').sidenav();
+//     $('.slider').slider({ 
+//         fullWidth: true, 
+//         indicators: true });
+//   });  
+
+//   autoplay();
+
+//   function autoplay() {
+//     $('slider').slider('next');
+//     setTimeout(autoplay, 8000);
+//   }
